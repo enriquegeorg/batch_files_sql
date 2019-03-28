f790e473d038f3c0e0b39d274f52f5f14359dd7e
enriquegeorg <enriquegeorg@gmail.com>
2019-03-28 08:59:17 -0300

    Add files via upload

--- extrator_parametros.bat
+++ extrator_parametros.bat
@@ -3,6 +3,19 @@ Title %~n0
 Mode 80,21 & Color A
 
 CHCP 65001
+
+SET location=%~dp0
+SET prereq=0
+IF EXIST %location%\Ferramentas\Extrator\queryoracle.sql  SET prereq=1
+IF EXIST %location%\Ferramentas\Extrator\querysql.sql  SET prereq=%prereq%2
+IF %prereq%==12 GOTO bdtype
+ECHO *************************************************************
+ECHO *****     VERIFIQUE OS PRE-REQUISITOS DO EXTRATOR    ********
+ECHO *****     E INICIE O EXECUTAVEL NOVAMENTE            ********
+ECHO *************************************************************
+PAUSE>nul
+EXIT
+
 :bdtype
 CLS
 ECHO *************************************************************
@@ -20,7 +33,6 @@ ECHO *********  INSIRA OS DADOS DE CONEXAO     *******************
 ECHO *************************************************************
 SET /p ip= *** ENDEREÇO DO SERVIDOR (C/ PORTA SE HOUVE):
 SET /p user= *** USUARIO DO BANCO:
-REM SET /p password= *** SENHA:
 Call:InputPassword "*** SENHA" password
 CLS
 ECHO *************************************************************
@@ -45,12 +57,6 @@ SET /p instance= *** INSTANCIA (SE TIVER):
 SET /p dbname=  *** NOME DA BASE DE DADOS:
 SET /p user= *** USUARIO DO BANCO:
 Call:InputPassword "*** SENHA" password
-REM SET /p password= *** SENHA:
-REM SET ip=qualidade.quirius.com.br
-REM SET instance=
-REM SET dbname=Governanca_8089
-REM SET user=sa
-REM SET password=@quirius123
 CLS
 ECHO *************************************************************
 ECHO *********  AGUARDE: TESTE DE CONEXAO      *******************
@@ -70,21 +76,21 @@ ECHO *************************************************************
 ECHO ****  1 - TERMINO DA IMPLANTACAO TECNICA       **************
 ECHO ****  2 - TERMINO DA PARAMETRIZACAO FUNCIONAL  **************
 ECHO ****  3 - GOLIVE                          *******************
-ECHO ****  4 - ANTES DA ATUALIZACAO            *******************
-ECHO ****  5 - APOS A ATUALIZACAO              *******************
+ECHO ****  4 - PRE-ATUALIZACAO                 *******************
+ECHO ****  5 - POS-ATUALIZACAO                 *******************
 ECHO *************************************************************
 ECHO *************************************************************
 SET /p etapa=****  ETAPA:
 
-SET location=%~dp0
+
 :loop
 IF %etapa% GEQ 6 GOTO valorerrado
 IF %etapa% LEQ 0 GOTO valorerrado
-IF %etapa% EQU 1 SET etapa=IMPLANTEC
-IF %etapa% EQU 2 SET etapa=PARAMFUNC
-IF %etapa% EQU 3 SET etapa=GOLIVE
-IF %etapa% EQU 4 SET etapa=PREATUALIZ
-IF %etapa% EQU 5 SET etapa=POSATUALIZ
+IF %etapa% EQU 1 SET etapa=1.IMPLANTEC
+IF %etapa% EQU 2 SET etapa=2.PARAMFUNC
+IF %etapa% EQU 3 SET etapa=3.GOLIVE
+IF %etapa% EQU 4 SET etapa=4.PREATUALIZACAO
+IF %etapa% EQU 5 SET etapa=5.POSATUALIZACAO
 GOTO start
 :valorerrado
 CLS
@@ -94,8 +100,8 @@ ECHO *************************************************************
 ECHO ****  1 - TERMINO DA IMPLANTACAO TECNICA       **************
 ECHO ****  2 - TERMINO DA PARAMETRIZACAO FUNCIONAL  **************
 ECHO ****  3 - GOLIVE                          *******************
-ECHO ****  4 - ANTES DA ATUALIZACAO            *******************
-ECHO ****  5 - APOS A ATUALIZACAO              *******************
+ECHO ****  4 - PRE-ATUALIZACAO                 *******************
+ECHO ****  5 - POS-ATUALIZACAO                 *******************
 ECHO *************************************************************
 ECHO *************************************************************
 ECHO *********  VALOR INCONSISTENTE!!!  **************************
@@ -116,72 +122,91 @@ SET hora=%tempo:~0,2%
 SET minuto=%tempo:~3,2%
 
 REM IF NOT EXIST %location%\extrator_parametros MKDIR %location%\extrator_parametros
-
-SET exlocation=%location%Backups\%dia%-%mes%-%ano%\extrator_parametros
+SET makelocation1=%location%Backups
+IF NOT EXIST %makelocation1% (
+  MKDIR %makelocation1%
+)
+SET makelocation2=%makelocation1%\%dia%-%mes%-%ano%
+IF NOT EXIST %makelocation2% (
+  MKDIR %makelocation2%
+)
+REM SET makelocation3=%makelocation1%\%dia%-%mes%-%ano%\extrator_parametros
+REM IF NOT EXIST %makelocation3% (
+REM   MKDIR %makelocation3%
+REM )
+SET makelocation4=%makelocation2%\%etapa%
+IF NOT EXIST %makelocation4% (
+  MKDIR %makelocation4%
+)
+SET exlocation=%makelocation4%
 REM ECHO %bkplocation%
 REM PAUSE>NUL
-IF NOT EXIST %exlocation% (
-  MKDIR %exlocation%
-)
 
 IF %bdtype% == 1 GOTO querysql
 IF %bdtype% == 2 GOTO queryora
 
 :querysql
-SET querydfeaplicacao=SELECT CAST(TDFE_PARAMETRO_APLICACAO.COD_PARAMETRO  AS VARCHAR(40)) AS COD_PARAMETRO, 	CAST(TDFE_PARAMETRO.DES_PARAMETRO AS VARCHAR(100)) AS DES_PARAMETRO, 	CAST(TDFE_PARAMETRO_APLICACAO.DES_VALOR AS VARCHAR(120)) AS DES_VALOR FROM TDFE_PARAMETRO_APLICACAO INNER JOIN TDFE_PARAMETRO ON(TDFE_PARAMETRO_APLICACAO.COD_PARAMETRO=TDFE_PARAMETRO.COD_PARAMETRO)
-SET querydfeempresa=SELECT CAST(TFRW_EMPRESA.COD_EMPRESA  AS VARCHAR(2)) AS COD_EMPRESA, CAST(TDFE_PARAMETRO_EMPRESA.COD_PARAMETRO  AS VARCHAR(40)) AS COD_PARAMETRO, CAST(TDFE_PARAMETRO.DES_PARAMETRO AS VARCHAR(80)) AS DES_PARAMETRO, CAST(TDFE_PARAMETRO_EMPRESA.DES_VALOR AS VARCHAR(40)) AS DES_VALOR FROM TDFE_PARAMETRO_EMPRESA INNER JOIN TFRW_EMPRESA ON(TDFE_PARAMETRO_EMPRESA.COD_EMPRESA=TFRW_EMPRESA.COD_EMPRESA) INNER JOIN TDFE_PARAMETRO ON(TDFE_PARAMETRO_EMPRESA.COD_PARAMETRO=TDFE_PARAMETRO.COD_PARAMETRO)
-SET querydfeestab=SELECT CAST(TDFE_PARAMETRO_ESTABELECIMENTO.COD_EMPRESA AS VARCHAR(10)) AS COD_EMPRESA, CAST(TDFE_PARAMETRO_ESTABELECIMENTO.COD_ESTABELECIMENTO AS VARCHAR(10)) AS COD_ESTABELECIMENTO, CAST(TDFE_PARAMETRO_NIVEL.COD_NIVEL AS VARCHAR(5)) AS COD_NIVEL, CAST(TDFE_PARAMETRO_ESTABELECIMENTO.COD_PARAMETRO AS VARCHAR(40)) AS COD_PARAMETRO, CAST(TDFE_PARAMETRO.DES_PARAMETRO AS VARCHAR(80)) AS DES_PARAMETRO, 	CAST(TDFE_PARAMETRO_ESTABELECIMENTO.DES_VALOR AS VARCHAR(40)) AS DES_VALOR FROM TDFE_PARAMETRO_ESTABELECIMENTO INNER JOIN TDFE_PARAMETRO ON(TDFE_PARAMETRO_ESTABELECIMENTO.COD_PARAMETRO=TDFE_PARAMETRO.COD_PARAMETRO) LEFT JOIN TDFE_PARAMETRO_NIVEL ON (TDFE_PARAMETRO_NIVEL.DES_TABLE='TDFE_PARAMETRO_ESTABELECIMENTO' AND TDFE_PARAMETRO.COD_NIVEL=TDFE_PARAMETRO_NIVEL.COD_NIVEL)
-SET querydfeconstr=SELECT DISTINCT(CAST(TDFE_ODBC_CONNECTION_STRING.CONNECTION_STRING AS VARCHAR(244))) AS CONNECTION_STRING, CAST(TDFE_ODBC_CONNECTION_STRING.ID_ODBC_CONNECTION_STRING AS VARCHAR(5)) AS ID_CONNECTION, CAST(TDFE_ODBC_ESTABELECIMENTO.COD_ESTABELECIMENTO AS VARCHAR(5)) AS COD_ESTAB,	CAST(TDFE_ODBC_ESTABELECIMENTO.COD_EMPRESA AS VARCHAR(5)) AS COD_EMPRESA FROM TDFE_ODBC_CONNECTION_STRING INNER JOIN TDFE_ODBC_ESTABELECIMENTO ON(TDFE_ODBC_CONNECTION_STRING.ID_ODBC_CONNECTION_STRING=TDFE_ODBC_ESTABELECIMENTO.ID_ODBC_CONNECTION_STRING) ORDER BY COD_EMPRESA, COD_ESTAB
-SET querydfeodbc=SELECT CAST(TDFE_ODBC.COD_EMPRESA AS VARCHAR(5)) AS COD_EMPRESA, CAST(TDFE_ODBC.COD_ODBC AS VARCHAR(30)) AS COD_ODBC, CAST(TDFE_ODBC.QUERY AS varchar(4020)) AS QUERY FROM TDFE_ODBC
-SET queryaudaplicacao=SELECT CAST(TAUD_PARAMETRO_APLICACAO.COD_PARAMETRO  AS VARCHAR(40)) AS COD_PARAMETRO, CAST(TAUD_PARAMETRO.DES_PARAMETRO AS VARCHAR(100)) AS DES_PARAMETRO, CAST(TAUD_PARAMETRO_APLICACAO.DES_VALOR AS VARCHAR(200)) AS DES_VALOR  FROM TAUD_PARAMETRO_APLICACAO INNER JOIN TAUD_PARAMETRO ON(TAUD_PARAMETRO_APLICACAO.COD_PARAMETRO=TAUD_PARAMETRO.COD_PARAMETRO)
-SET queryaudestab=SELECT CAST(TAUD_PARAMETRO_ESTABELECIMENTO.COD_EMPRESA  AS VARCHAR(10)) AS COD_EMPRESA, CAST(TAUD_PARAMETRO_ESTABELECIMENTO.COD_ESTABELECIMENTO  AS VARCHAR(10)) AS COD_ESTABELECIMENTO, CAST(TAUD_PARAMETRO_ESTABELECIMENTO.COD_PARAMETRO  AS VARCHAR(40)) AS COD_PARAMETRO, CAST(TAUD_PARAMETRO.DES_PARAMETRO AS VARCHAR(80)) AS DES_PARAMETRO, CAST(TAUD_PARAMETRO_ESTABELECIMENTO.DES_VALOR AS VARCHAR(40)) AS DES_VALOR FROM TAUD_PARAMETRO_ESTABELECIMENTO INNER JOIN TAUD_PARAMETRO ON(TAUD_PARAMETRO_ESTABELECIMENTO.COD_PARAMETRO=TAUD_PARAMETRO.COD_PARAMETRO)
 
-SET queryteste=SELECT CAST_ODBC AS VARC33HAR(30)) AS COD_ODBC, REPLACE(REPLA345CE(CAST(TDFE_ODBC.QUERY AS VARCHAR(500)), CHAR(5), ' ' ),CHAR(4), ' ' ) AS TESTE FROM TDFE_ODBC
+REM SET queryteste=SELECT CAST_ODBC AS VARC33HAR(30)) AS COD_ODBC, REPLACE(REPLA345CE(CAST(TDFE_ODBC.QUERY AS VARCHAR(500)), CHAR(5), ' ' ),CHAR(4), ' ' ) AS TESTE FROM TDFE_ODBC
 
 ECHO *************************************************************
 ECHO *********       EXECUTANDO... AGUARDE     *******************
 ECHO *************************************************************
 
-sqlcmd -S %ip%\%instance% -U %user% -P %password% -d %dbname% -Q  "%querydfeaplicacao%" -b -s " " -o %exlocation%\TDFE_PARAMETRO_APLICACAO_%ano%%mes%%dia%_%hora%%minuto%_%etapa%.txt
+SET tabela=querydfeaplicacao
+sqlcmd -S %ip%\%instance% -U %user% -P %password% -d %dbname% %variables% -i %location%Ferramentas\Extrator\querysql.sql -b -s " " -o %exlocation%\TDFE_PARAMETRO_APLICACAO.txt
 IF ERRORLEVEL 1 (
   ECHO *************************************************************
   ECHO ******  ERRO NA EXTRACAO: TDFE_PARAMETRO_APLICACAO   ********
   PAUSE>nul |set/p = ******  APERTE ENTER PARA CONTINUAR !!               ********
 )
-sqlcmd -S %ip%\%instance% -U %user% -P %password% -d %dbname% -Q  "%querydfeempresa%" -b -s " " -o %exlocation%\TDFE_PARAMETRO_EMPRESA_%ano%%mes%%dia%_%hora%%minuto%_%etapa%.txt
+SET tabela=querydfeempresa
+sqlcmd -S %ip%\%instance% -U %user% -P %password% -d %dbname% %variables% -i %location%Ferramentas\Extrator\querysql.sql -b -s " " -o %exlocation%\TDFE_PARAMETRO_EMPRESA.txt
 IF ERRORLEVEL 1 (
   ECHO *************************************************************
   ECHO ******  ERRO NA EXTRACAO: TDFE_PARAMETRO_EMPRESA     ********
   PAUSE>nul |set/p = ******  APERTE ENTER PARA CONTINUAR !!               ********
 )
-sqlcmd -S %ip%\%instance% -U %user% -P %password% -d %dbname% -Q "%querydfeestab%" -b -s " " -o %exlocation%\TDFE_PARAMETRO_ESTABELECIMENTO_%ano%%mes%%dia%_%hora%%minuto%_%etapa%.txt
+SET tabela=querydfeestab
+sqlcmd -S %ip%\%instance% -U %user% -P %password% -d %dbname% %variables% -i %location%Ferramentas\Extrator\querysql.sql -b -s " " -o %exlocation%\TDFE_PARAMETRO_ESTABELECIMENTO.txt
 IF ERRORLEVEL 1 (
   ECHO *************************************************************
   ECHO ******  ERRO NA EXTRACAO: TDFE_PARAMETRO_ESTABELECIMENTO   **
   PAUSE>nul |set/p = ******  APERTE ENTER PARA CONTINUAR !!               ********
 )
-sqlcmd -S %ip%\%instance% -U %user% -P %password% -d %dbname% -Q "%querydfeconstr%" -b -s " " -o %exlocation%\TDFE_ODBC_ESTABELECIMENTO_%ano%%mes%%dia%_%hora%%minuto%_%etapa%.txt
+SET tabela=querydfeconstr
+sqlcmd -S %ip%\%instance% -U %user% -P %password% -d %dbname% %variables% -i %location%Ferramentas\Extrator\querysql.sql -b -s " " -o %exlocation%\TDFE_ODBC_ESTABELECIMENTO.txt
 IF ERRORLEVEL 1 (
   ECHO *************************************************************
   ECHO ******  ERRO NA EXTRACAO: TDFE_ODBC_ESTABELECIMENTO   *******
   PAUSE>nul |set/p = ******  APERTE ENTER PARA CONTINUAR !!               ********
 )
-sqlcmd -S %ip%\%instance% -U %user% -P %password% -d %dbname% -Q "%querydfeodbc%" -b -s " " -b -o %exlocation%\TDFE_ODBC_%ano%%mes%%dia%_%hora%%minuto%_%etapa%.txt
+SET tabela=querydfeodbc
+sqlcmd -S %ip%\%instance% -U %user% -P %password% -d %dbname% %variables% -i %location%Ferramentas\Extrator\querysql.sql -b -s " " -b -o %exlocation%\TDFE_ODBC.txt
 IF ERRORLEVEL 1 (
   ECHO *************************************************************
   ECHO ******  ERRO NA EXTRACAO: TDFE_ODBC                   *******
   PAUSE>nul |set/p = ******  APERTE ENTER PARA CONTINUAR !!               ********
 )
-sqlcmd -S %ip%\%instance% -U %user% -P %password% -d %dbname% -Q "%queryaudaplicacao%" -b -s " " -o %exlocation%\TAUD_PARAMETRO_APLICACAO_%ano%%mes%%dia%_%hora%%minuto%_%etapa%.txt
+SET tabela=queryaudaplicacao
+sqlcmd -S %ip%\%instance% -U %user% -P %password% -d %dbname% %variables% -i %location%Ferramentas\Extrator\querysql.sql -b -s " " -o %exlocation%\TAUD_PARAMETRO_APLICACAO.txt
 IF ERRORLEVEL 1 (
   ECHO *************************************************************
   ECHO ******  ERRO NA EXTRACAO: TAUD_PARAMETRO_APLICACAO    *******
   PAUSE>nul |set/p = ******  APERTE ENTER PARA CONTINUAR !!               ********
 )
-sqlcmd -S %ip%\%instance% -U %user% -P %password% -d %dbname% -Q "%queryaudestab%" -b -s " " -o %exlocation%\TAUD_PARAMETRO_ESTABELECIMENTO_%ano%%mes%%dia%_%hora%%minuto%_%etapa%.txt
+SET tabela=queryaudestab
+sqlcmd -S %ip%\%instance% -U %user% -P %password% -d %dbname% %variables% -i %location%Ferramentas\Extrator\querysql.sql  -b -s " " -o %exlocation%\TAUD_PARAMETRO_ESTABELECIMENTO.txt
 IF ERRORLEVEL 1 (
   ECHO *************************************************************
   ECHO ******  ERRO NA EXTRACAO: TAUD_PARAMETRO_ESTABELECIMENTO  ***
+  PAUSE>nul |set/p = ******  APERTE ENTER PARA CONTINUAR !!               ********
+)
+SET tabela=msgaplicacao
+sqlcmd -S %ip%\%instance% -U %user% -P %password% -d %dbname% %variables% -i %location%Ferramentas\Extrator\querysql.sql -b -s " " -o %exlocation%\TDFE_GRUPO_DISTRIBUICAO.txt
+IF ERRORLEVEL 1 (
+  ECHO *************************************************************
+  ECHO ******  ERRO NA EXTRACAO: TDFE_GRUPO_DISTRIBUICAO    ********
   PAUSE>nul |set/p = ******  APERTE ENTER PARA CONTINUAR !!               ********
 )
 
@@ -189,15 +214,17 @@ GOTO finish
 
 :queryora
 ECHO *************************************************************
-ECHO *********  FUNCAO NAO IMPLEMENTADA        *******************
+ECHO *********       EXECUTANDO... AGUARDE     *******************
 ECHO *************************************************************
-REM SET querydfeaplicacao=SELECT TO_CHAR(TDFE_PARAMETRO_APLICACAO.COD_PARAMETRO (40)) AS COD_PARAMETRO, 	TO_CHAR(TDFE_PARAMETRO.DES_PARAMETRO(100)) AS DES_PARAMETRO, 	TO_CHAR(TDFE_PARAMETRO_APLICACAO.DES_VALOR(120)) AS DES_VALOR FROM TDFE_PARAMETRO_APLICACAO INNER JOIN TDFE_PARAMETRO ON(TDFE_PARAMETRO_APLICACAO.COD_PARAMETRO=TDFE_PARAMETRO.COD_PARAMETRO)
-REM SET querydfeempresa=SELECT TO_CHAR(TFRW_EMPRESA.COD_EMPRESA (2)) AS COD_EMPRESA, TO_CHAR(TDFE_PARAMETRO_EMPRESA.COD_PARAMETRO (40)) AS COD_PARAMETRO, TO_CHAR(TDFE_PARAMETRO.DES_PARAMETRO(80)) AS DES_PARAMETRO, TO_CHAR(TDFE_PARAMETRO_EMPRESA.DES_VALOR(40)) AS DES_VALOR FROM TDFE_PARAMETRO_EMPRESA INNER JOIN TFRW_EMPRESA ON(TDFE_PARAMETRO_EMPRESA.COD_EMPRESA=TFRW_EMPRESA.COD_EMPRESA) INNER JOIN TDFE_PARAMETRO ON(TDFE_PARAMETRO_EMPRESA.COD_PARAMETRO=TDFE_PARAMETRO.COD_PARAMETRO)
-REM SET querydfeestab=SELECT TO_CHAR(TDFE_PARAMETRO_ESTABELECIMENTO.COD_EMPRESA (10)) AS COD_EMPRESA, TO_CHAR(TDFE_PARAMETRO_ESTABELECIMENTO.COD_ESTABELECIMENTO(10)) AS COD_ESTABELECIMENTO, TO_CHAR(TDFE_PARAMETRO_ESTABELECIMENTO.COD_PARAMETRO (40)) AS COD_PARAMETRO, TO_CHAR(TDFE_PARAMETRO.DES_PARAMETRO(80)) AS DES_PARAMETRO, TO_CHAR(TDFE_PARAMETRO_ESTABELECIMENTO.DES_VALOR(40)) AS DES_VALOR FROM TDFE_PARAMETRO_ESTABELECIMENTO INNER JOIN TDFE_PARAMETRO ON(TDFE_PARAMETRO_ESTABELECIMENTO.COD_PARAMETRO=TDFE_PARAMETRO.COD_PARAMETRO)
-REM SET querydfeconstr=SELECT DISTINCT(TO_CHAR(TDFE_ODBC_CONNECTION_STRING.CONNECTION_STRING(244))) AS CONNECTION_STRING, TO_CHAR(TDFE_ODBC_CONNECTION_STRING.ID_ODBC_CONNECTION_STRING(5)) AS ID_CONNECTION, TO_CHAR(TDFE_ODBC_ESTABELECIMENTO.COD_ESTABELECIMENTO(5)) AS COD_ESTAB,	TO_CHAR(TDFE_ODBC_ESTABELECIMENTO.COD_EMPRESA(5)) AS COD_EMPRESA FROM TDFE_ODBC_CONNECTION_STRING INNER JOIN TDFE_ODBC_ESTABELECIMENTO ON(TDFE_ODBC_CONNECTION_STRING.ID_ODBC_CONNECTION_STRING=TDFE_ODBC_ESTABELECIMENTO.ID_ODBC_CONNECTION_STRING) ORDER BY COD_EMPRESA, COD_ESTAB
-REM SET querydfeodbc=SELECT TO_CHAR(TDFE_ODBC.COD_EMPRESA(5)) AS COD_EMPRESA, TO_CHAR(TDFE_ODBC.COD_ODBC(30)) AS COD_ODBC, TO_CHAR(TDFE_ODBC.QUERY(4020)) AS QUERY FROM TDFE_ODBC
-REM SET queryaudaplicacao=SELECT TO_CHAR(TAUD_PARAMETRO_APLICACAO.COD_PARAMETRO (40)) AS COD_PARAMETRO, TO_CHAR(TAUD_PARAMETRO.DES_PARAMETRO(100)) AS DES_PARAMETRO, TO_CHAR(TAUD_PARAMETRO_APLICACAO.DES_VALOR(200)) AS DES_VALOR  FROM TAUD_PARAMETRO_APLICACAO INNER JOIN TAUD_PARAMETRO ON(TAUD_PARAMETRO_APLICACAO.COD_PARAMETRO=TAUD_PARAMETRO.COD_PARAMETRO)
-REM SET queryaudestab=SELECT TO_CHAR(TAUD_PARAMETRO_ESTABELECIMENTO.COD_EMPRESA (10)) AS COD_EMPRESA, TO_CHAR(TAUD_PARAMETRO_ESTABELECIMENTO.COD_ESTABELECIMENTO (10)) AS COD_ESTABELECIMENTO, TO_CHAR(TAUD_PARAMETRO_ESTABELECIMENTO.COD_PARAMETRO (40)) AS COD_PARAMETRO, TO_CHAR(TAUD_PARAMETRO.DES_PARAMETRO(80)) AS DES_PARAMETRO, TO_CHAR(TAUD_PARAMETRO_ESTABELECIMENTO.DES_VALOR(40)) AS DES_VALOR FROM TAUD_PARAMETRO_ESTABELECIMENTO INNER JOIN TAUD_PARAMETRO ON(TAUD_PARAMETRO_ESTABELECIMENTO.COD_PARAMETRO=TAUD_PARAMETRO.COD_PARAMETRO)
+SET querydfeaplicacao=%exlocation%\TDFE_PARAMETRO_APLICACAO.txt
+SET querydfeempresa=%exlocation%\TDFE_PARAMETRO_EMPRESA.txt
+SET querydfeestab=%exlocation%\TDFE_PARAMETRO_ESTABELECIMENTO.txt
+SET querydfeconstr=%exlocation%\TDFE_ODBC_ESTABELECIMENTO.txt
+SET querydfeodbc=%exlocation%\TDFE_ODBC.txt
+SET queryaudaplicacao=%exlocation%\TAUD_PARAMETRO_APLICACAO.txt
+SET queryaudestab=%exlocation%\TAUD_PARAMETRO_ESTABELECIMENTO.txt
+SET msgaplicacao=%exlocation%\TDFE_GRUPO_DISTRIBUICAO.txt
+ECHO EXIT | sqlplus %user%/%password%@%ip% @%location%Ferramentas\Extrator\queryoracle.sql '%querydfeaplicacao%' '%querydfeempresa%' '%querydfeestab%' '%querydfeconstr%' '%querydfeodbc%' '%queryaudaplicacao%' '%queryaudestab%' '%msgaplicacao%'
 
 :finish
 ECHO *************************************************************
